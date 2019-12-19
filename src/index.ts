80f4843770cea217e6b9bdc63d3e10e3b56ea8a4
Niclas Lindgren <niclaslindgren91@hotmail.com>
2019-12-20 00:03:16 +0200

    Add express hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+import express from 'express'
+
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.get('/asd', (req, res) => {
+  res.send('asd')
+})
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`)
+})
