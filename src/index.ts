8517676fdc5d0e60c5c805afb3dde2b7731c85d0
Robert <robert@ekholmgroup.fi>
2019-12-21 23:27:07 +0200

    Add game entity

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser'
 import express from 'express'
 import mongoose from 'mongoose'
 import Player from './Player'
+import Game from './Game'
 
 const app = express()
 const port = 8080
@@ -14,12 +15,24 @@ app.get('/players', (req, res) => {
   Player.find().then(players => res.send(players.map(({ name }) => ({ name }))))
 })
 
+app.get('/games', (req, res) => {
+  Game.find().then(games => res.send(games.map(({ name }) => ({ name }))))
+})
+
 app.post('/players/save', (req, res) => {
   new Player(req.body)
     .save()
     .then(() => res.send('Saved'))
     .catch(() => res.send('Failed'))
 })
+
+app.post('/games/save', (req, res) => {
+  new Game(req.body)
+    .save()
+    .then(() => res.send('Saved'))
+    .catch(() => res.send('Failed'))
+})
+
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`)
