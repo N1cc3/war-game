70811c03871d284e54a1c76d054c0506c164ab57
Niclas Lindgren <niclaslindgren91@hotmail.com>
2019-12-21 23:17:05 +0200

    Add get persons endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
+app.get('/players', (req, res) => {
+  Player.find().then(players => res.send(players.map(({ name }) => ({ name }))))
 })
 
 app.post('/players/save', (req, res) => {
