29a5ce321682eef4e9696ac84c3941edc262b4dd
Robert <robert@ekholmgroup.fi>
2019-12-23 00:47:00 +0200

    Add neighbor test and move Cube to Hex

--- src/hexgrid/Hex.test.ts
+++ src/hexgrid/Hex.test.ts
@@ -1,8 +1,20 @@
 import Hex from './Hex'
 
-test('Checks the value of parity', () => {
-  expect(new Hex(1, 0).parity()).toBe(0)
-  expect(new Hex(1, 1).parity()).toBe(1)
-  expect(new Hex(1, 2).parity()).toBe(0)
-  expect(new Hex(1, 3).parity()).toBe(1)
+describe('Hex', () => {
+  test('Checks the value of parity', () => {
+    expect(new Hex(1, 0).parity()).toBe(0)
+    expect(new Hex(1, 1).parity()).toBe(1)
+    expect(new Hex(1, 2).parity()).toBe(0)
+    expect(new Hex(1, 3).parity()).toBe(1)
+  })
+
+  test('neighbors should return the 6 neighboring hexes', () => {
+    const origin = new Hex(0, 0)
+    const neighbors = origin.neighbors()
+
+    expect(neighbors).toHaveLength(6)
+    expect(neighbors).not.toContain(new Hex(0, 0))
+    expect(neighbors.find(hex => hex.equals(new Hex(0, 1)))).toBeTruthy()
+    expect(neighbors.find(hex => hex.equals(new Hex(1, 0)))).toBeTruthy()
+  })
 })
--- src/hexgrid/Hex.ts
+++ src/hexgrid/Hex.ts
@@ -1,5 +1,4 @@
 import { range } from '../utils'
-import Cube from './Cube'
 
 export default class Hex {
   public static of = ({ x, y }: { x: number; y: number }) => new Hex(x, y)
@@ -12,8 +11,25 @@ export default class Hex {
   public add = (that: Hex) => new Hex(this.x + that.x, this.y + that.y)
   public sub = (that: Hex) => new Hex(this.x - that.x, this.y - that.y)
   public toCube = () => new Cube(this.x - (this.y - this.parity()) / 2, this.y, -this.x - this.y)
+  public equals = (that: Hex) => this.x === that.x && this.y === that.y
 }
 
 const evenDirections = [new Hex(1, 0), new Hex(0, -1), new Hex(-1, -1), new Hex(-1, 0), new Hex(-1, 1), new Hex(0, 1)]
 const oddDirections = [new Hex(1, 0), new Hex(1, -1), new Hex(0, -1), new Hex(-1, 0), new Hex(0, 1), new Hex(1, 1)]
 const directions = [evenDirections, oddDirections]
+
+const abs = Math.abs
+const max = Math.max
+const min = Math.min
+
+class Cube {
+  constructor(public x: number, public y: number, public z: number) {}
+  public distanceTo = (that: Cube) => (abs(this.x - that.x) + abs(this.y - that.y) + abs(this.z - that.z)) / 2
+  public neighborsWithin = (radius: number) =>
+    range(-radius, radius).flatMap(x =>
+      range(max(-radius, -x - radius), min(radius, -x + radius)).flatMap(y => this.add(new Cube(x, y, -x - y))),
+    )
+  public add = (that: Cube) => new Cube(this.x + that.x, this.y + that.y, this.z + that.z)
+  public sub = (that: Cube) => new Cube(this.x - that.x, this.y - that.y, this.z - that.z)
+  public toHex = () => new Hex(this.x + (this.z - (this.z % 2)) / 2, this.z)
+}
