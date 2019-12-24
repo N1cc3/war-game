ce9e854c613831d638040782b07f759ec79bfd87
Niclas Lindgren <niclaslindgren91@hotmail.com>
2019-12-24 03:14:53 +0200

    Make game be used as mutable

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,9 +1,14 @@
 import { Game } from './domain'
 import { simulateTick } from './game'
 
+const emptyGame: Game = { lastTickTime: 0, tickTime: 0, units: [], unitTypes: [], players: [] }
+
 describe('game', () => {
   test('lastTickTime updates on simulateTick', () => {
-    const game: Game = { lastTickTime: 0, tickTime: 100 }
-    expect(simulateTick(game)).toMatchObject<Game>({ lastTickTime: 100, tickTime: 100 })
+    const game: Game = { ...emptyGame, tickTime: 100 }
+
+    simulateTick(game)
+
+    expect(game).toMatchObject<Game>({ ...emptyGame, lastTickTime: 100, tickTime: 100 })
   })
 })
