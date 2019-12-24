c7e9704956ec15373e2ee2a5024e77877389e73c
Niclas Lindgren <niclaslindgren91@hotmail.com>
2019-12-24 04:33:15 +0200

    Add unit combat

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,14 +1,43 @@
+import Hex from '../hexgrid/Hex'
+
 export default class Game {
   constructor(
     readonly tickTime: number, // Time between ticks, seconds
     private _lastTickTime: number, // When last tick was simulated, epoch seconds
+    private _units: Unit[] = []
   ) {}
 
-  get lastTickTime() {
-    return this._lastTickTime
-  }
+  readonly lastTickTime = () => this._lastTickTime
+  readonly units = () => this._units
 
-  public simulate = () => {
+  readonly simulate = () => {
+    this._units.forEach(this.fight)
     this._lastTickTime += this.tickTime
   }
+
+  private fight = (attacker: Unit) => {
+    const target = this._units.find(
+      defender =>
+        defender.owner !== attacker.owner && // is enemy
+        defender.position.equals(attacker.position) && // on same hex
+        defender.hp > 0 // is alive
+    )
+    if (target) target.hp -= attacker.type.damage
+  }
+}
+
+export type Unit = {
+  type: UnitType
+  owner: Player
+  position: Hex
+  hp: number
+}
+export type UnitType = {
+  name: string
+  damage: number
+  maxHp: number
+  speed: number
+}
+export type Player = {
+  name: string
 }
